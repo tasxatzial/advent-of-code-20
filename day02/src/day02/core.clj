73973e9cbbc3c8606f7d1663ffadb93dce3610ff
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-04 23:01:09 +0200

    day2: problem 1

--- day02/src/day02/core.clj
+++ day02/src/day02/core.clj
@@ -29,6 +29,32 @@
   [seq]
   (get seq 2))
 
+; ---------------------------------------
+; problem 1
+
+(defn get-minmax
+  "Returns a vector of the minimum and maximum numbers in the password policy.
+   Input is a vector in the list returned by parse()."
+  [seq]
+  (let [[min max] (clojure.string/split (get seq 0) #"-")]
+    (map str->int [min max])))
+
+(defn isValid1?
+  "Returns true of false depending on whether the password is valid according
+  to the password policy."
+  [seq]
+  (let [[min max] (get-minmax seq)
+        letter (get-letter seq)
+        password (get-password seq)
+        occurences (re-seq (re-pattern (str letter)) password)]
+    (and (>= (count occurences) min) (<= (count occurences) max))))
+
+; ---------------------------------------
+; results
+
+(def day02-1
+  (count (filter true? (map isValid1? (parse (slurp input))))))
+
 (defn -main
   []
-  )
+  (println day02-1)) ;469
