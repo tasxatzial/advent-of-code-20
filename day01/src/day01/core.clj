398962d154b7e5dd14c767106e8c4a843c25f822
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-04 22:55:50 +0200

    day1: add basic parsing and spliting functions

---
+++ day01/src/day01/core.clj
@@ -0,0 +1,21 @@
+(ns day01.core
+  (:gen-class))
+
+; --------------------------
+; common
+
+(def input "resources\\input.txt")
+
+(defn str->int
+  "Converts a string to integer."
+  [^String s]
+  (Integer. s))
+
+(defn parse
+  "Splits the input string by \n and converts it into a list of numbers."
+  [s]
+  (map str->int (clojure.string/split s #"\n")))
+
+(defn -main
+  []
+ )
