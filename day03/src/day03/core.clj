3d9cd31cb69428dc51ae651a21722d961f118e5c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-04 23:03:21 +0200

    day3: add basic parsing and spliting functions

---
+++ day03/src/day03/core.clj
@@ -0,0 +1,16 @@
+(ns day03.core
+  (:gen-class))
+
+; ---------------------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse
+  "Splits the input string by \n."
+  [s]
+  (clojure.string/split s #"\n"))
+
+(defn -main
+  []
+  )
