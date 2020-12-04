ef3248a41da6e4f748b8da630c72e8b3cdc66ff9
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-04 23:24:08 +0200

    day4: add basic parsing and spliting functions

---
+++ day04/src/day04/core.clj
@@ -0,0 +1,38 @@
+(ns day04.core
+  (:gen-class))
+
+; ---------------------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(def required-keys '(:byr :iyr :eyr :hgt :hcl :ecl :pid))
+
+(defn parse
+  "Splits the input string by \n\n and then splits again by space.
+  Returns a list of vectors, each element in the vector is
+  one field (key:value) of the passport."
+  [s]
+  (let [password-strings (clojure.string/split s #"\n\n")]
+    (map #(clojure.string/split % #"[ \n]") password-strings)))
+
+(defn get-passport-field
+  "Creates a (key value) map from a passport field."
+  [x]
+  (let [[key val] (clojure.string/split x #":")]
+    {(keyword key) val}))
+
+(defn create-passport
+  "Creates a passport map from a passport string."
+  [passport]
+  (reduce (fn [passport field]
+            (into passport (get-passport-field field)))
+          {}
+          passport))
+
+; put all passports in a list
+(def passports (map create-passport (parse (slurp input-file))))
+
+(defn -main
+  []
+  )
