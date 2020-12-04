3e5329538458cf695eb4f2efbc5143026cba3279
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-04 23:05:05 +0200

    day3: problem 2

--- day03/src/day03/core.clj
+++ day03/src/day03/core.clj
@@ -30,11 +30,37 @@
             seq)))
 
 ; ---------------------------------------
+; problem 2
+
+(defn count-trees2
+  "counts the number of trees encountered when we move
+  right steps and down steps. Result is the first element in the returned vector."
+  [seq right down]
+  (let [part-length (count (first seq))
+        tree (.charAt "#" 0)]
+    (reduce (fn [[trees index] part]
+              (let [next-index (mod (+ index right) part-length)]
+                (if (= tree (get part index))
+                  [(inc trees) next-index]
+                  [trees next-index])))
+            [0 0]
+            (take-nth down seq))))
+
+; ---------------------------------------
 ; results
 
 (def day03-1
   (first (count-trees1 (parse (slurp input-file)))))
 
+(def day03-2
+  (let [parsed-input (parse (slurp input-file))]
+    [(first (count-trees2 parsed-input 1 1))
+     (first (count-trees2 parsed-input 3 1))
+     (first (count-trees2 parsed-input 5 1))
+     (first (count-trees2 parsed-input 7 1))
+     (first (count-trees2 parsed-input 1 2))]))
+
 (defn -main
   []
-  (println day03-1)) ;276
+  (println day03-1)  ;276
+  (println day03-2)) ;[100 276 85 90 37]
