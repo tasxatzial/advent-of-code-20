3c1762f3d1d5ad3619bbce4d231a377dc82aaf34
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-04 22:58:05 +0200

    day1: problem 2

--- day01/src/day01/core.clj
+++ day01/src/day01/core.clj
@@ -42,13 +42,53 @@
     (let [res (add-to-first-element seq)]
       (or res (sum2020-1 (rest seq))))))
 
+; --------------------------
+; problem 2
+
+(defn add-to-two-elements
+  "Uses two fixed numbers f & i and finds a number from seq such that the sum of all
+  numbers is 2020. Returns the first triplet that sums to 2020 or nil if such triplet
+  cannot be found."
+  ([seq f i] (add-to-two-elements seq f i (inc i)))
+  ([seq f i j]
+   (if (= j (count seq))
+     nil
+     (let [g (first (drop i seq)) h (first (drop j seq)) sum (+ f g h)]
+       (if (= sum 2020)
+         [f g h]
+         (if (< sum 2020)                                   ;optimization
+           (add-to-two-elements seq f i (inc j))))))))
+
+(defn add-to-one-element
+  "Uses a fixed number f and finds two numbers from seq such that the sum of all
+  numbers is 2020. Returns the first triplet that sums to 2020 or nil if such triplet
+  cannot be found. Assumes seq is sorted in increasing order."
+  ([seq f] (add-to-one-element seq f 0))
+  ([seq f i]
+   (if (or (= i (- (count seq) 1)) (> (+ f i) 2020))                 ;optimization
+     nil
+     (let [res (add-to-two-elements seq f i)]
+       (or res (add-to-one-element seq f (inc i)))))))
+
+(defn sum2020-2
+  "Returns the first triplet from seq that sums to 2020 or nil if such triplet
+  cannot be found. Assumes seq is sorted in increasing order."
+  [seq]
+  (if (or (= 2 (count seq)) (> (first seq) 673))            ;optimization
+    nil
+    (let [res (add-to-one-element seq (first seq))]
+      (or res (sum2020-2 (rest seq))))))
+
 ; ---------------------------------------
 ; results
 
 (def day01-1
   (sum2020-1 (sort (parse (slurp input)))))
 
+(def day01-2
+  (sum2020-2 (sort (parse (slurp input)))))
+
 (defn -main
   []
- (println day01-1) ;[279 1741]
-  )
+  (println day01-1) ;[279 1741]
+  (println day01-2)) ;[257 494 1269]
