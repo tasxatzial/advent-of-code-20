92d569c060bd649a99435f7bb5b409a2f6a5ac1e
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-04 23:04:15 +0200

    day3: problem 1

--- day03/src/day03/core.clj
+++ day03/src/day03/core.clj
@@ -11,6 +11,30 @@
   [s]
   (clojure.string/split s #"\n"))
 
+; ---------------------------------------
+; problem 1
+
+(defn count-trees1
+  "counts the number of trees encountered when we move 3 steps right
+  and one step down. Result is the first element in the returned vector."
+  [seq]
+  (let [part-length (count (first seq))
+        tree (.charAt "#" 0)
+        right 3]
+    (reduce (fn [[trees index] part]
+              (let [next-index (mod (+ index right) part-length)]
+                (if (= tree (get part index))
+                  [(inc trees) next-index]
+                  [trees next-index])))
+            [0 0]
+            seq)))
+
+; ---------------------------------------
+; results
+
+(def day03-1
+  (first (count-trees1 (parse (slurp input-file)))))
+
 (defn -main
   []
-  )
+  (println day03-1)) ;276
