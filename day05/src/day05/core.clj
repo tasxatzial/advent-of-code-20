a41eb43a13c9a76733a8670af98a52971f4f7b45
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-05 12:36:26 +0200

    day5: add a function that finds the row number

--- day05/src/day05/core.clj
+++ day05/src/day05/core.clj
@@ -10,6 +10,20 @@
   [s]
   (clojure.string/split s #"\n"))
 
+; ---------------------------------------
+; problem 1
+
+(defn find-row
+  "Finds the row number given a seat code."
+  [code-string]
+  (let [row-code (take 7 code-string)]
+    (first (reduce (fn [[front back] letter]
+                     (if (= letter '\F)
+                       [front (dec (/ (inc (+ front back)) 2))]
+                       [(/ (inc (+ front back)) 2) back]))
+                   [0 127]
+                   row-code))))
+
 (defn -main
   []
   (println (parse (slurp input-file))))
