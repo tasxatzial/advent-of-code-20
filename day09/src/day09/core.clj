457edd670e7993f9caf64de1efc92d1102fb9d69
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-10 00:31:42 +0200

    day9 problem 2

--- day09/src/day09/core.clj
+++ day09/src/day09/core.clj
@@ -92,10 +92,35 @@
          checked-num)))))
 
 ; ---------------------------------------
+; problem 2
+
+(def first-num (find-first))
+
+(defn find-smallest-largest
+  "Finds a contiguous list of numbers in the input file such that the sum of
+  all numbers in the list equals to the solution to problem 1."
+  ([] (find-smallest-largest 0 0 0))
+  ([prev-sum first-index second-index]
+   (if (= first-index count-col)
+     nil
+     (if (>= second-index count-col)
+       (let [next-first-index (inc first-index)]
+         (recur (nth parsed-input next-first-index) next-first-index (inc next-first-index)))
+       (let [new-sum (+ prev-sum (nth parsed-input second-index))]
+         (if (= first-num new-sum)
+           (concat (take (inc (- second-index first-index)) (drop first-index parsed-input)))
+           (recur new-sum first-index (inc second-index))))))))
+
+; ---------------------------------------
 ; results
 
-(def day09-1 (find-first))
+(def day09-1 first-num)
+
+(def day09-2
+  (let [sorted-sublist (sort (find-smallest-largest))]
+    (+ (first sorted-sublist) (last sorted-sublist))))
 
 (defn -main
   []
-  (println day09-1))                                        ;393911906
+  (println day09-1)                                         ;393911906
+  (println day09-2))                                        ;59341885
