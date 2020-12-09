0dbe2601959f1e6dcb3ed810f9cbc71196c273f1
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-09 22:56:23 +0200

    day9 problem 1: add a function that inserts a number to a sorted col

--- day09/src/day09/core.clj
+++ day09/src/day09/core.clj
@@ -17,7 +17,23 @@
   (clojure.string/split s #"\n"))
 
 ; convert the input file to a list of BigInteger
-(def parsed-input (map str->BigInt (parse (slurp input-file))))
+(def parsed-input (apply vector (map str->BigInt (parse (slurp input-file)))))
+
+(def preamble-size 25)
+
+; ---------------------------------------
+; problem 1
+
+(defn sorted-insert
+  "Adds num to a sorted col so that they new col is also sorted."
+  ([sorted-col num]
+   (sorted-insert sorted-col num '()))
+  ([sorted-col num result]
+   (if (empty? sorted-col)
+     (concat result [num])
+     (if (> (first sorted-col) num)
+       (concat result [num] sorted-col)
+       (recur (rest sorted-col) num (concat result [(first sorted-col)]))))))
 
 (defn -main
   []
