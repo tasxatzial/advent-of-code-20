021429b22fb2bb7b1bea094456cbb165f19dc774
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-10 08:36:52 +0200

    day10 problem 1

--- day10/src/day10/core.clj
+++ day10/src/day10/core.clj
@@ -12,12 +12,26 @@
   (Integer. s))
 
 (defn parse
-  "Splits the input string by \n."
+  "Splits the input string by \n and converts the strings to numbers."
   [s]
   (map #(str->Int %) (clojure.string/split s #"\n")))
 
 (def parsed-input (parse (slurp input-file)))
 
+; ---------------------------------------
+; problem 1
+
+; ---------------------------------------
+; results
+
+(def day10-1
+  (let [sorted-col (sort parsed-input)
+        shifted-col (conj (butlast sorted-col) 0)
+        diff-col (map #(- %1 %2) sorted-col shifted-col)
+        count-1jolts (count (filter #(= 1 %) diff-col))
+        count-3jolts (count (filter #(= 3 %) diff-col))]
+    (* count-1jolts (inc count-3jolts))))    ;+1 because our device always has diff 3
+
 (defn -main
   []
-  (println parsed-input))
+  (println day10-1))
