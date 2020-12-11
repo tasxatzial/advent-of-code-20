5cdc1039db7a6e3580737dc665387552679d84a9
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-11 22:55:32 +0200

    day11: change the seat data structure to vector for faster access

--- day11/src/day11/core.clj
+++ day11/src/day11/core.clj
@@ -1,6 +1,9 @@
 (ns day11.core
   (:gen-class))
 
+; ---------------------------------------
+; common
+
 (def input-file "resources\\input.txt")
 
 (defn parse
@@ -24,10 +27,10 @@
    :bottom-left [(inc row-index) (dec col-index)]
    :bottom-right [(inc row-index) (inc col-index)]})
 
-(defn compute-seat-key
-  "Returns the key of a seat in position [row-index, col-index]."
+(defn compute-seat-index
+  "Returns the index of a seat at coordinates [row-index, col-index]."
   [row-index col-index]
-  (keyword (str (+ col-index (* row-index (inc max-col))))))
+  (+ col-index (* row-index (inc max-col))))
 
 (defn process-adjacents-pos
   "Accepts a map of adjacents as returned by find-adjacents-pos() and creates
@@ -41,7 +44,7 @@
                        (<= row-index max-row)
                        (>= col-index 0)
                        (<= col-index max-col))
-                (conj result (compute-seat-key row-index col-index))
+                (conj result (compute-seat-index row-index col-index))
                 result)))
           '()
           adjacents))
@@ -49,18 +52,22 @@
 (defn process-input
   "Processes the input returned by parse() and creates the appropriate structure
   that represents the seats and their adjacent seats."
-  ([] (process-input {} 0 0))
+  ([] (process-input [] 0 0))
   ([result row-index col-index]
    (if (> row-index max-row)
      result
      (if (> col-index max-col)
        (recur result (inc row-index) 0)
-       (let [seat-key (compute-seat-key row-index col-index)
-             adjacents (process-adjacents-pos (find-adjacents-pos row-index col-index))
+       (let [adjacents (process-adjacents-pos (find-adjacents-pos row-index col-index))
              seat-state (get (get parsed-input row-index) col-index)
-             result (conj result {seat-key [seat-state adjacents]})]
+             result (conj result [seat-state adjacents])]
          (recur result row-index (inc col-index)))))))
+
+(def seat-map (process-input))
+
+; ---------------------------------------
+; problem 1
 
 (defn -main
   []
-  (println (process-input)))
+  (println seat-map))
