1bdc118e4a385a5345e81dad6a7dbba200e128d0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-11 22:33:29 +0200

    day11: add a function that creates the seat keys from the adjacent coordinates

--- day11/src/day11/core.clj
+++ day11/src/day11/core.clj
@@ -24,6 +24,28 @@
    :bottom-left [(inc row-index) (dec col-index)]
    :bottom-right [(inc row-index) (inc col-index)]})
 
+(defn compute-seat-key
+  "Returns the key of a seat in position [row-index, col-index]."
+  [row-index col-index]
+  (keyword (str (+ col-index (* row-index (inc max-col))))))
+
+(defn process-adjacents-pos
+  "Accepts a map of adjacents as returned by find-adjacents-pos() and creates
+  a list of seat keys that correspond to these adjacents. Invalid positions from
+  the map are not taken into account."
+  [adjacents]
+  (reduce (fn [result [pos-name adjacent-pos]]
+            (let [row-index (first adjacent-pos)
+                  col-index (second adjacent-pos)]
+              (if (and (>= row-index 0)
+                       (<= row-index max-row)
+                       (>= col-index 0)
+                       (<= col-index max-col))
+                (conj result (compute-seat-key row-index col-index))
+                result)))
+          '()
+          adjacents))
+
 (defn -main
   []
   (println (process-input)))
