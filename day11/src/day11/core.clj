44441a51f00bc778507377930e031b78ed8945bb
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-11 22:31:25 +0200

    day11: add a function that returns the adjacent coordinates of (row,col)

--- day11/src/day11/core.clj
+++ day11/src/day11/core.clj
@@ -8,6 +8,22 @@
   [s]
   (clojure.string/split s #"\n"))
 
+(def parsed-input (parse (slurp input-file)))
+(def max-row (dec (count parsed-input)))
+(def max-col (dec (count (first parsed-input))))
+
+(defn find-adjacents-pos
+  "Returns a map of the adjacent coordinates of [row-index col-index]."
+  [row-index col-index]
+  {:left [row-index (dec col-index)]
+   :right [row-index (inc col-index)]
+   :top [(dec row-index) col-index]
+   :bottom [(inc row-index) col-index]
+   :top-left [(dec row-index) (dec col-index)]
+   :top-right [(dec row-index) (inc col-index)]
+   :bottom-left [(inc row-index) (dec col-index)]
+   :bottom-right [(inc row-index) (inc col-index)]})
+
 (defn -main
   []
-  (println (parse (slurp input-file))))
+  (println (process-input)))
