f24944b85743e569c0d395704832a21edfbfebbc
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-12 16:43:04 +0200

    day12: parse the input file into the appropriate data structure

--- day12/src/day12/core.clj
+++ day12/src/day12/core.clj
@@ -6,6 +6,10 @@
 
 (def input-file "resources\\input.txt")
 
+(defn str->int
+  [^String s]
+  (Integer. s))
+
 (defn parse
   "Splits the input string by \n."
   [s]
@@ -13,6 +17,13 @@
 
 (def parsed-input (parse (slurp input-file)))
 
+; create the appropriate structure for the instructions
+(def instructions
+  (reduce (fn [result instruction]
+            (conj result [(keyword (str (first instruction))) (str->int (apply str (drop 1 instruction)))]))
+          []
+          parsed-input))
+
 (defn -main
   []
-  (println parsed-input))
+  (println instructions))
