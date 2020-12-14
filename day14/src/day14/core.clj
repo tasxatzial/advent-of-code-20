07d76ee73e21c2781e5c90e5e8bb64305edcefae
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-14 16:56:26 +0200

    day14 problem 1: create the appropriate structures for the mask/memory lines

--- day14/src/day14/core.clj
+++ day14/src/day14/core.clj
@@ -45,8 +45,41 @@
                    (first mask))]
        (apply-mask (rest mask) (rest value) (conj result new-value))))))
 
+(def partitioned-input (partition-by mask? parsed-input))
+
 ; ---------------------------------------
 ; problem 1
+
+(defn process-mem-block
+  "Creates a map of memory addresses to value from a block of memory instructions,
+  these are all the instructions between two masks in the input file."
+  [block]
+  (reduce (fn [result mem-string]
+            (conj result (parse-mem mem-string)))
+          []
+          block))
+
+; the vector of the masks in the input file
+(def masks
+  (reduce (fn [result line]
+            (if (mask? (first line))
+              (conj result (parse-mask (first line)))
+              result))
+          []
+          partitioned-input))
+
+; a vector of maps, each map has the instructions of the corresponding mask from the above masks
+(def mems
+  (reduce (fn [result line]
+            (if (mask? (first line))
+              result
+              (conj result (process-mem-block line))))
+          []
+          partitioned-input))
+
+; the final docking instructions
+(def docking-instructions
+  (map #(vector %1 %2) masks mems))
 
 ; ---------------------------------------
 ; results
