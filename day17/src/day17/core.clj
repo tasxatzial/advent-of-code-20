9c91de5713c85374d78edf219acf2a5a8d0f96a1
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-17 19:33:35 +0200

    day17: create a keyword from integer coordinates and vice versa

--- day17/src/day17/core.clj
+++ day17/src/day17/core.clj
@@ -3,12 +3,27 @@
 
 (def input-file "resources\\input.txt")
 
+(defn str->int
+  "Convert a string to integer."
+  [^String s]
+  (Integer. s))
+
 (defn parse
   "Splits the input string by \n."
   [s]
   (clojure.string/split s #"\n"))
 
 (def input (parse (slurp input-file)))
+
+(defn gen-key
+  "Generates a keyword from given integers x, y, z."
+  [x y z]
+  (keyword (str x "-" y "-" z)))
+
+(defn gen-coordinates
+  "Generates the integer x,y,z coordinates from the specified keyword."
+  [keyword]
+  (map str->int (rest (clojure.string/split (str keyword) #":|-"))))
 
 (defn -main
   []
