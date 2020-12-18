e0f8b511fe81ce8d207be99aaf3410802fd2a2e0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-18 21:12:21 +0200

    day18: add basic functions

---
+++ day18/src/day18/core.clj
@@ -0,0 +1,18 @@
+(ns day18.core
+  (:gen-class))
+
+; ---------------------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse
+  "Splits the input string by \n."
+  [s]
+  (clojure.string/split s #"\n"))
+
+(def input (parse (slurp input-file)))
+
+(defn -main
+  []
+  (println input))
