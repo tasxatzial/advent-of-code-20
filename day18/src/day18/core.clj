3dc5dc7cfd1553675c66bf317569d9611d925b5d
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-19 01:51:51 +0200

    day18: add a function that returns a sub expression

--- day18/src/day18/core.clj
+++ day18/src/day18/core.clj
@@ -13,6 +13,18 @@
 
 (def input (parse (slurp input-file)))
 
+(defn find-subexpr
+  "Expr must start with (. Everything until the matching ) will be returned."
+  ([expr] (find-subexpr (rest expr) [] 1))
+  ([expr result count]
+   (if (or (empty? expr) (= 0 count))
+     (butlast result)
+     (let [char (first expr)]
+       (case char
+         \( (recur (rest expr) (conj result \() (inc count))
+         \) (recur (rest expr) (conj result \)) (dec count))
+         (recur (rest expr) (conj result char) count))))))
+
 (defn -main
   []
-  (println input))
+  (println ))
