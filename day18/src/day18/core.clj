1d5d33a61525d8fd4df43c4c30203173ffaf05a6
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-19 02:12:11 +0200

    day18 problem 1: add a function that evaluates an expression

--- day18/src/day18/core.clj
+++ day18/src/day18/core.clj
@@ -25,6 +25,42 @@
          \) (recur (rest expr) (conj result \)) (dec count))
          (recur (rest expr) (conj result char) count))))))
 
+(defn calculate-expr
+  "Calculates an expression."
+  ([expr] (calculate-expr expr [1 (eval *)]))
+  ([expr result]
+   (if (empty? expr)
+     result
+     (let [char (first expr)]
+       (case char
+         \( (let [subexpr (find-subexpr expr)
+                  subexpr-value (calculate-expr subexpr)
+                  op (second result)
+                  value1 (first subexpr-value)
+                  value2 (first result)
+                  new-result (vector (op value1 value2) op)
+                  new-expr (drop (+ 2 (count subexpr)) expr)]
+              (calculate-expr new-expr new-result))
+         \+ (let [new-expr (rest expr)
+                  value (first result)
+                  new-result (vector value (eval +))]
+              (calculate-expr new-expr new-result))
+         \* (let [new-expr (rest expr)
+                  res (first result)
+                  new-result (vector res (eval *))]
+              (calculate-expr new-expr new-result))
+         \space (let [new-expr (rest expr)]
+                  (calculate-expr new-expr result))
+         (let [new-expr (rest expr)
+               op (second result)
+               value1 (first result)
+               value2 (Integer. (str char))
+               new-result (vector (op value1 value2) \space)]
+           (calculate-expr new-expr new-result)))))))
+
+; ---------------------------------------
+; results
+
 (defn -main
   []
-  (println ))
+  (println (calculate-expr "6 + (4 * 8)")))
