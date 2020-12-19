766c089ad097c20d2fd6690f4b424b5249a1d1a6
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-19 02:17:57 +0200

    day18 problem 1: compute solution

--- day18/src/day18/core.clj
+++ day18/src/day18/core.clj
@@ -25,42 +25,48 @@
          \) (recur (rest expr) (conj result \)) (dec count))
          (recur (rest expr) (conj result char) count))))))
 
-(defn calculate-expr
+; ---------------------------------------
+; problem 1
+
+(defn calculate-expr1
   "Calculates an expression."
-  ([expr] (calculate-expr expr [1 (eval *)]))
+  ([expr] (calculate-expr1 expr [1 (eval *)]))
   ([expr result]
    (if (empty? expr)
      result
      (let [char (first expr)]
        (case char
          \( (let [subexpr (find-subexpr expr)
-                  subexpr-value (calculate-expr subexpr)
+                  subexpr-value (calculate-expr1 subexpr)
                   op (second result)
                   value1 (first subexpr-value)
                   value2 (first result)
                   new-result (vector (op value1 value2) op)
                   new-expr (drop (+ 2 (count subexpr)) expr)]
-              (calculate-expr new-expr new-result))
+              (calculate-expr1 new-expr new-result))
          \+ (let [new-expr (rest expr)
                   value (first result)
                   new-result (vector value (eval +))]
-              (calculate-expr new-expr new-result))
+              (calculate-expr1 new-expr new-result))
          \* (let [new-expr (rest expr)
                   res (first result)
                   new-result (vector res (eval *))]
-              (calculate-expr new-expr new-result))
+              (calculate-expr1 new-expr new-result))
          \space (let [new-expr (rest expr)]
-                  (calculate-expr new-expr result))
+                  (calculate-expr1 new-expr result))
          (let [new-expr (rest expr)
                op (second result)
                value1 (first result)
                value2 (Integer. (str char))
                new-result (vector (op value1 value2) \space)]
-           (calculate-expr new-expr new-result)))))))
+           (calculate-expr1 new-expr new-result)))))))
 
 ; ---------------------------------------
 ; results
 
+(def day18-1
+  (apply + (map (comp first calculate-expr1) input)))
+
 (defn -main
   []
-  (println (calculate-expr "6 + (4 * 8)")))
+  (println day18-1))                                        ;98621258158412
