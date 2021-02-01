c8ddfebf853cbc99f03beda4297fc75de798b200
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-02-01 23:41:42 +0200

    day20: add functions that return the sides of a tile

--- day20/src/day20/core.clj
+++ day20/src/day20/core.clj
@@ -22,6 +22,35 @@
 (def tiles
   (into {} (map parse-tile (parse (slurp input-file)))))
 
+(defn get-top-side
+  "Returns the top side of a tile."
+  [tile]
+  (map identity (first tile)))
+
+(defn get-bottom-side
+  "Returns the bottom side of a tile."
+  [tile]
+  (map identity (last tile)))
+
+(defn get-left-side
+  "Returns the left side of a tile."
+  [tile]
+  (map first tile))
+
+(defn get-right-side
+  "Returns the right side of a tile."
+  [tile]
+  (map last tile))
+
+(defn get-tile-sides
+  "Returns a map that contains all sides of a tile."
+  [tile]
+  (let [top-side (get-top-side tile)
+        bottom-side (get-bottom-side tile)
+        left-side (get-bottom-side tile)
+        right-side (get-right-side tile)]
+    (assoc {} :top top-side :bottom bottom-side :left left-side :right right-side)))
+
 (defn -main
   []
   (println tiles))
