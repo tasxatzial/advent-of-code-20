31c425a08340e9a220be3b3deb06c69a84c967e7
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-02-01 22:49:24 +0200

    day20: parse the input file into the appropriate structure

--- day20/src/day20/core.clj
+++ day20/src/day20/core.clj
@@ -1,7 +1,27 @@
 (ns day20.core
   (:gen-class))
 
+; ---------------------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse
+  "Splits the input string by \n."
+  [s]
+  (clojure.string/split s #"\n\n"))
+
+(defn parse-tile
+  "Parses a string tile (and its legend) to the appropriate map structure."
+  [input-image]
+  (let [tile-lines (clojure.string/split input-image #"\n")
+        tile-text (first tile-lines)
+        parsed-tile-text (clojure.string/split tile-text #"[ |:]")]
+    {(Integer. ^String (second parsed-tile-text)) (rest tile-lines)}))
+
+(def tiles
+  (into {} (map parse-tile (parse (slurp input-file)))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println tiles))
