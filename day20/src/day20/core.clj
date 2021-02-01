cfe8911cb2fa762209dd8d4cfa32f8d43512a5a8
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-02-01 23:55:28 +0200

    day20: the tile structure now includes only its sides

--- day20/src/day20/core.clj
+++ day20/src/day20/core.clj
@@ -11,17 +11,6 @@
   [s]
   (clojure.string/split s #"\n\n"))
 
-(defn parse-tile
-  "Parses a string tile (and its legend) to the appropriate map structure."
-  [input-image]
-  (let [tile-lines (clojure.string/split input-image #"\n")
-        tile-text (first tile-lines)
-        parsed-tile-text (clojure.string/split tile-text #"[ |:]")]
-    {(Integer. ^String (second parsed-tile-text)) (rest tile-lines)}))
-
-(def tiles
-  (into {} (map parse-tile (parse (slurp input-file)))))
-
 (defn get-top-side
   "Returns the top side of a tile."
   [tile]
@@ -47,10 +36,23 @@
   [tile]
   (let [top-side (get-top-side tile)
         bottom-side (get-bottom-side tile)
-        left-side (get-bottom-side tile)
+        left-side (get-left-side tile)
         right-side (get-right-side tile)]
     (assoc {} :top top-side :bottom bottom-side :left left-side :right right-side)))
 
+(defn parse-tile
+  "Parses a string tile (and its legend) to the appropriate map structure.
+  Key is the tile id, value is a list of rows"
+  [input-image]
+  (let [tile-lines (clojure.string/split input-image #"\n")
+        tile-text (first tile-lines)
+        tile (rest tile-lines)
+        parsed-tile-text (clojure.string/split tile-text #"[ |:]")]
+    {(Integer. ^String (second parsed-tile-text)) (get-tile-sides tile)}))
+
+(def tiles-sides
+  (into {} (map parse-tile (parse (slurp input-file)))))
+
 (defn -main
   []
-  (println tiles))
+  (println tiles-sides))
