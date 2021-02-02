0533bbb08649e2c46577f7bf5a30bc6a9a394827
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-02-02 14:22:57 +0200

    day22: create a vector of card numbers for each player

--- day22/src/day22/core.clj
+++ day22/src/day22/core.clj
@@ -1,7 +1,29 @@
 (ns day22.core
   (:gen-class))
 
+; ---------------------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse
+  "Splits the input string by \n\n"
+  [s]
+  (clojure.string/split s #"\n\n"))
+
+(def input (parse (slurp input-file)))
+
+(defn gen-cards
+  "Accepts a string describing either player1 data or player2 data. This string is one of
+  the two items from the list returned by parse(). Returns a vector of numbers that describes
+  a player's cards."
+  [s]
+  (let [data (rest (clojure.string/split s #"\n"))]
+    (mapv #(Integer. ^String %) data)))
+
+(def player1-cards (gen-cards (first input)))
+(def player2-cards (gen-cards (second input)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println player2-cards))
