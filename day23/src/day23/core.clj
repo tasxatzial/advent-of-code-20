0037ceef77991f02f782222bb7da65a88c887c99
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-02-03 20:25:51 +0200

    day23: extract the digits of an integer into a list

--- day23/src/day23/core.clj
+++ day23/src/day23/core.clj
@@ -1,7 +1,15 @@
 (ns day23.core
   (:gen-class))
 
+(def input 418976235)
+
+(defn num-to-seq
+  "Returns a list of the digits of the integer num."
+  [num]
+  (->> num
+       str
+       (map (comp read-string str))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println (num-to-seq input)))
