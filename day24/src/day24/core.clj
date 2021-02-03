3a6c56993eb3aae8e1cce03ca783710cd910a118
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-02-04 00:43:11 +0200

    day24: parse a string of directions

--- day24/src/day24/core.clj
+++ day24/src/day24/core.clj
@@ -1,7 +1,33 @@
 (ns day24.core
   (:gen-class))
 
+; ---------------------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse
+  "Splits the input string by \n"
+  [s]
+  (clojure.string/split s #"\n"))
+
+(def input (parse (slurp input-file)))
+
+(def two-letter-directions (hash-set "se" "sw" "nw" "ne"))
+
+(defn parse-directions
+  "Parses a string that contains the directions to a tile into a vector."
+  ([directions]
+   (parse-directions directions []))
+  ([tile result]
+   (if (empty? tile)
+     result
+     (let [first-letter (str (first tile))
+           two-letters (str first-letter (second tile))]
+       (if (contains? two-letter-directions two-letters)
+         (recur (drop 2 tile) (conj result two-letters))
+         (recur (rest tile) (conj result first-letter)))))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println input))
