2da2ec169664e8c0cea1578183bd3807db0c305a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-02-04 01:32:08 +0200

    day24: find the position of tile that is identified by a directions list

--- day24/src/day24/core.clj
+++ day24/src/day24/core.clj
@@ -15,6 +15,9 @@
 
 (def two-letter-directions (hash-set "se" "sw" "nw" "ne"))
 
+(def move-instructions
+  {"w" [-1 0] "e" [1 0] "se" [1 -1] "sw" [-1 -1] "nw" [-1 1] "ne" [1 1]})
+
 (defn parse-directions
   "Parses a string that contains the directions to a tile into a vector."
   ([directions]
@@ -28,6 +31,20 @@
          (recur (drop 2 tile) (conj result two-letters))
          (recur (rest tile) (conj result first-letter)))))))
 
+(defn get-tile
+  "Returns the [x y] of the tile that is identified by
+  the specified list of directions."
+  ([directions]
+   (get-tile directions [0 0]))
+  ([directions position]
+   (if (empty? directions)
+     position
+     (reduce (fn [[x y] direction]
+               (let [instructions (get move-instructions direction)]
+                 [(+ (first instructions) x) (+ (second instructions) y)]))
+             position
+             directions))))
+
 (defn -main
   []
-  (println input))
+  (println (get-tile (parse-directions "wwwnwwwwwwwwenwwwwwwww"))))
