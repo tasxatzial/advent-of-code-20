63b95aec2ab85be6f98817b6f7a744ac476618d8
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-02-07 23:24:17 +0200

    day21: parse the input into an appropriate structure

--- day21/src/day21/core.clj
+++ day21/src/day21/core.clj
@@ -1,7 +1,29 @@
 (ns day21.core
   (:gen-class))
 
+(def input-file "resources\\input.txt")
+
+(defn parse
+  "Splits the input string by \n."
+  [s]
+  (clojure.string/split s #"\n"))
+
+(def input (parse (slurp input-file)))
+
+(defn parse-line
+  "Parses an input line and returns a vector of two items.
+  First item is a list of the known ingredients. Second item is a vector
+  of the unknown ingredients."
+  [line]
+  (let [ingredients (clojure.string/split line #"[(|)]")
+        unknown-ingredients (clojure.string/split (first ingredients) #" ")
+        known-ingredients (->> (clojure.string/split (second ingredients) #"[,| ]")
+                               (filter (comp boolean seq) )
+                               rest)]
+    [known-ingredients unknown-ingredients]))
+
+(def parsed-input (map parse-line input))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println parsed-input))
