b3a8b8c5f08705ae8f0c3754581b635c6f2ce9b5
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-02-08 02:55:25 +0200

    day21 problem1: add functions that update the possible ingredients that can be an allergen

--- day21/src/day21/core.clj
+++ day21/src/day21/core.clj
@@ -1,5 +1,9 @@
 (ns day21.core
+  (:require [clojure.set])
   (:gen-class))
+
+; ---------------------------------------
+; common
 
 (def input-file "resources\\input.txt")
 
@@ -20,9 +24,35 @@
         known-ingredients (->> (clojure.string/split (second ingredients) #"[,| ]")
                                (filter (comp boolean seq) )
                                rest)]
-    [known-ingredients unknown-ingredients]))
+    [known-ingredients (set unknown-ingredients)]))
 
 (def parsed-input (map parse-line input))
+
+; ---------------------------------------
+; problem 1
+
+(defn update-possible-ingredients
+  "First argument is a vector of two items: a list of known allergens and a set of
+  all ingredients (for a food). Second argument is a map of {allergen -> all possible
+  ingredients} for each known allergen.
+  Returns a new map of {allergen -> all ingredients} for each known allergen."
+  [[known-allergens unknown-allergens] allergen-map]
+  (reduce (fn [result allergen]
+            (let [current-unknown (get allergen-map allergen)]
+              (if current-unknown
+                (assoc result allergen (clojure.set/intersection current-unknown unknown-allergens))
+                (assoc result allergen unknown-allergens))))
+          {}
+          known-allergens))
+
+(defn update-all-possible-ingredients
+  "Returns a map of {allergen -> possible ingredients} for every known allergen."
+  []
+  (reduce (fn [result [allergen ingredients]]
+            (let [new-ingredients (update-possible-ingredients [allergen ingredients] result)]
+              (into result new-ingredients)))
+          {}
+          parsed-input))
 
 (defn -main
   []
