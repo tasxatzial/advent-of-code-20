abb2453c105c28978f534016d1968458f0a07c66
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-02-11 11:22:01 +0200

    day20: create a tile struct from an entry in the input file

--- day20/src/day20/core.clj
+++ day20/src/day20/core.clj
@@ -7,52 +7,49 @@
 (def input-file "resources\\input.txt")
 
 (defn parse
-  "Splits the input string by \n."
+  "Splits the input string by \n\n"
   [s]
   (clojure.string/split s #"\n\n"))
 
-(defn get-top-side
-  "Returns the top side of a tile."
-  [tile]
-  (map identity (first tile)))
-
-(defn get-bottom-side
-  "Returns the bottom side of a tile."
-  [tile]
-  (map identity (last tile)))
-
-(defn get-left-side
-  "Returns the left side of a tile."
-  [tile]
-  (map first tile))
-
-(defn get-right-side
-  "Returns the right side of a tile."
-  [tile]
-  (map last tile))
-
-(defn get-tile-sides
-  "Returns a map that contains all sides of a tile."
-  [tile]
-  (let [top-side (get-top-side tile)
-        bottom-side (get-bottom-side tile)
-        left-side (get-left-side tile)
-        right-side (get-right-side tile)]
-    (assoc {} :top top-side :bottom bottom-side :left left-side :right right-side)))
-
-(defn parse-tile
-  "Parses a string tile (and its legend) to the appropriate map structure.
-  Key is the tile id, value is a list of rows"
-  [input-image]
-  (let [tile-lines (clojure.string/split input-image #"\n")
+(defn create-tile
+  "Returns a [tile-number tile-image] that represents a tile.
+  tile-image is a list of strings (one string per line)."
+  [input]
+  (let [tile-lines (clojure.string/split input #"\n")
         tile-text (first tile-lines)
         tile (rest tile-lines)
         parsed-tile-text (clojure.string/split tile-text #"[ |:]")]
-    {(Integer. ^String (second parsed-tile-text)) (get-tile-sides tile)}))
+    [(Integer. ^String (second parsed-tile-text)) tile]))
 
-(def tiles-sides
-  (into {} (map parse-tile (parse (slurp input-file)))))
+(defn get-top-side
+  "Returns the top side of a tile."
+  [tile-image]
+  (map identity (first tile-image)))
+
+(defn get-bottom-side
+  "Returns the bottom side of a tile."
+  [tile-image]
+  (map identity (last tile-image)))
+
+(defn get-left-side
+  "Returns the left side of a tile."
+  [tile-image]
+  (map first tile-image))
+
+(defn get-right-side
+  "Returns the right side of a tile."
+  [tile-image]
+  (map last tile-image))
+
+(defn get-tile-sides
+  "Returns a map that contains all sides of a tile."
+  [time-image]
+  (let [top-side (get-top-side time-image)
+        bottom-side (get-bottom-side time-image)
+        left-side (get-left-side time-image)
+        right-side (get-right-side time-image)]
+    (assoc {} :top top-side :bottom bottom-side :left left-side :right right-side)))
 
 (defn -main
   []
-  (println tiles-sides))
+  (println (create-tile (first (parse (slurp input-file))))))
