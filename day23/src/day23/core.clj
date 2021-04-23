7c77acbb1cfe734c7ced58592771d05b310c0e40
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-04-23 23:11:56 +0300

    day23: group code for problem 1

--- day23/src/day23/core.clj
+++ day23/src/day23/core.clj
@@ -13,7 +13,14 @@
        str
        (map (comp read-string str))))
 
-(defn find-dest-cup
+(def initial-cups (num-to-seq input))
+
+; ---------------------------------------
+; problem 1
+
+(def max-cup_1 9)
+
+(defn find-dest-cup_1
   "Finds the final destination cup using dest as the initial destination."
   [dest picked-cups max-cup]
   (if (= dest 0)
@@ -24,11 +31,10 @@
       (recur (dec dest) picked-cups max-cup)
       dest)))
 
-(defn get-index
-  "Returns the index of num in col. Returns -1 if num does not appear
-  anywhere in col."
-  ([num col]
-   (get-index num col 0))
+(defn get-index_1
+  "Returns the index of num in coll or -1 if num is not found."
+  ([num coll]
+   (get-index_1 num coll 0))
   ([num col index]
    (if (empty? col)
      -1
@@ -36,39 +42,36 @@
        index
        (recur num (rest col) (inc index))))))
 
-(defn play-game
-  "Plays total max-moves of the crab game."
-  [cups max-moves max-cup]
-  (if (= 0 max-moves)
+(defn play-game_1
+  "Makes total-moves in the crab game."
+  [cups total-moves max-cup]
+  (if (= 0 total-moves)
     cups
     (let [current-cup (first cups)
           picked-cups (take 3 (rest cups))
           rest-cups (drop 4 cups)
-          dest-cup (find-dest-cup (dec current-cup) picked-cups max-cup)
-          dest-index (inc (get-index dest-cup rest-cups))
+          dest-cup (find-dest-cup_1 (dec current-cup) picked-cups max-cup)
+          dest-index (inc (get-index_1 dest-cup rest-cups))
           new-seq (concat (take dest-index rest-cups) picked-cups (drop dest-index rest-cups) (list current-cup))]
-      (recur new-seq (dec max-moves) max-cup))))
+      (recur new-seq (dec total-moves) max-cup))))
 
-; ---------------------------------------
-; problem 1
-
-(def cups1 (num-to-seq input))
-(def max-cup1 9)
-
-(defn collect-cups1
+(defn collect-cups_1
   "Collects in clockwise fashion all cup labels starting after the cup labeled 1.
   Returns the labels concatenated in a string."
   [cups]
-  (let [index-one (get-index 1 cups)
+  (let [index-one (get-index_1 1 cups)
         new-cups (concat (drop index-one cups) (take index-one cups))]
     (apply str (rest new-cups))))
+
+; ---------------------------------------
+; problem 2
 
 ; ---------------------------------------
 ; results
 
 (defn day23-1
   []
-  (collect-cups1 (play-game cups1 100 max-cup1)))
+  (collect-cups_1 (play-game_1 initial-cups 100 max-cup_1)))
 
 (defn -main
   []
